1edf810e077d59b7268d2e11b77b7de53b0c9eb4
Mansurte <arielgonzaloalonso@gmail.com>
2022-02-11 16:40:59 -0300

    initial commit in dev branch

--- main.go
+++ main.go
@@ -12,8 +12,12 @@ var (
 
 func main() {
 	inter = 67
+	var a bool
+	a = true
 	var j string
+	//var s2 string
 	j = strconv.Itoa(inter)
 	//j := 40
 	fmt.Printf("%v, %T \n", j, j)
+	fmt.Printf("%v, %T\n", a, a)
 }
